f6ede911cfa7cad9af3e19a9009892d9cd5d4b26
Atish Pawar <147304634+atish-2023@users.noreply.github.com>
2025-02-06 14:26:11 +0530

    Create program3.java

---
+++ usingstatement/program3.java
@@ -0,0 +1,61 @@
+// for update the data from the database table
+
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class program3 {
+	
+	private static final String url = "jdbc:mysql://127.0.0.1:3306/mydevice";
+	private static final String user = "root";
+	private static final String password = "Atish@1193";
+
+	public static void main(String[] args) {
+		
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("driver loaded successfully");
+			
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try 
+		{
+			Connection conn = DriverManager.getConnection(url,user,password);
+			
+			
+			Statement st = conn.createStatement();
+			
+			
+			
+			
+			int count  = st.executeUpdate(String.format("update mobile set  network = '%s' where name = '%s'","5G","nokia"));
+			
+			if(count > 0 )
+			{
+				System.out.println("data updated  successfully ");
+			}
+			else {
+				System.out.println("not data updated successfully");
+			}
+		}
+		catch(SQLException e)
+		{
+		   e.printStackTrace();
+		}
+		
+		
+		
+		
+
+	}
+
+}
